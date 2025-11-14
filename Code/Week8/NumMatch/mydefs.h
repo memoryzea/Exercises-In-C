528f43e497104de633c5a303df8b4b0dbf90f1ca
memoryzea <91736892+memoryzea@users.noreply.github.com>
2025-11-14 09:21:44 +0000

    Add files via upload

--- Code/Week8/NumMatch/mydefs.h
+++ Code/Week8/NumMatch/mydefs.h
@@ -4,3 +4,27 @@
    your struct board, helper function
    prototypes etc.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define ROWS 4
+#define COLS 5
+#define MAX_BOARDS 100000
+
+typedef struct board{
+    int graid[ROWS][COLS];
+}board;
+
+#include "nm.h"
+
+/* Helper function prototypes */
+bool is_valid_move(board *b, pair z);
+bool is_touching_or_aligned(board *b, pair z);
+bool boards_equal(board *a, board *b);
+bool is_final(board *b);
+void print_board(board *b);
+
+
